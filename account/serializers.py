e042363b91ef302df13a78efbdf55c66aef87790
tturdumamatovv <muki2022@mail.ru>
2023-06-04 16:36:40 +0600

    templates

--- account/serializers.py
+++ account/serializers.py
@@ -198,3 +198,29 @@ class PasswordResetSerializer(_PasswordResetSerializer):
             raise serializers.ValidationError('Invalid e-mail address')
 
         return value
+
+
+class MobilePasswordResetSerializer(_PasswordResetSerializer):
+
+    def save(self):
+        request = self.context.get('request')
+        opts = {
+            'use_https': request.is_secure(),
+            'from_email': getattr(settings, 'EMAIL_HOST_USER'),
+
+            'email_template_name': 'reset_password.html',
+
+            'request': request
+        }
+        self.reset_form.save(**opts)
+
+    def validate_email(self, value):
+        # Create PasswordResetForm with the serializer
+        self.reset_form = self.password_reset_form_class(data=self.initial_data)
+        if not self.reset_form.is_valid():
+            raise serializers.ValidationError(_('Error'))
+
+        if not User.objects.filter(email=value).exists():
+            raise serializers.ValidationError('Invalid e-mail address')
+
+        return value
