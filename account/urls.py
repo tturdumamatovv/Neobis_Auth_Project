4cc0b5432d367b28bb420a55a526ebff29ea096a
tturdumamatovv <muki2022@mail.ru>
2023-06-07 21:37:10 +0600

    for mobile

--- account/urls.py
+++ account/urls.py
@@ -1,18 +1,20 @@
 from django.urls import path, re_path
 
 from .views import RegistrationView, VerifyEmail, LoginAPIView, PasswordTokenCheckAPI, RequestPasswordResetEmail, \
-    SetNewPasswordAPIView, LogoutAPIView, UpdateRegisterView, UpdatePasswordView
+    SetNewPasswordAPIView, LogoutAPIView, UpdateRegisterView, UpdatePasswordView, VerifyEmailMobile, RegistrationViewForMobile
 
 from rest_auth.views import PasswordResetView, PasswordResetConfirmView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
+    path('register-for-mobile/', RegistrationViewForMobile.as_view(), name='register-for-mobile'),
     path('register-update/', UpdateRegisterView.as_view(), name='register-update'),
     path('password-update/', UpdatePasswordView.as_view(), name='password-update'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('logout/', LogoutAPIView.as_view(), name='logout'),
     path('email-verify/', VerifyEmail.as_view(), name='email-verify'),
+    path('email-verify-for-mobile/', VerifyEmailMobile.as_view(), name='email-verify-for-mobile'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
     path('password_reset_confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
--- account/views.py
+++ account/views.py
@@ -43,6 +43,27 @@ class RegistrationView(generics.GenericAPIView):
         return Response(user_data, status=status.HTTP_201_CREATED)
 
 
+class RegistrationViewForMobile(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request):
+        user = request.data
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        user_data = serializer.data
+        user = User.objects.get(email=user_data['email'])
+        token = RefreshToken.for_user(user).access_token
+        current_site = get_current_site(request).domain
+        relativeLink = reverse('email-verify-for-mobile')
+        absurl = 'http://'+current_site+relativeLink+'?token='+str(token)
+        email_body = 'Hi '+user.email+' Use link below to verify your email \n' + absurl
+        data = {'email_body': email_body, 'to_email': user.email, 'email_subject': 'Verify your email'}
+        Util.send_email(data)
+
+        return Response(user_data, status=status.HTTP_201_CREATED)
+
+
 class UpdateRegisterView(generics.GenericAPIView):
     serializer_class = RegisterUpdateSerializer
 
